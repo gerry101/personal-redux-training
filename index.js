04099398d15a0a582ef3c70c45ce02f081183585
Gerry <gerrymigwi6@gmail.com>
2018-12-02 12:35:58 +0200

    Added functionality to get state

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// Initialized an empty repository.
-// All changes to the project will be made in subsequent branches.
+function createStore() {
+	// The store should have four parts:
+	// 1. The state.
+	// 2. A way to get the state.
+	// 3. A way to listen to changes on the state.
+	// 4. A way to update the state.
 
-// Happy hacking!
+	let state
+
+	const getState = () => state
+
+	return {
+		getState
+	}
+}
