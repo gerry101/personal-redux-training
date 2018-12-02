b5aee73dbfc986c61313f2c564f6b15f8c8c654f
Gerry <gerrymigwi6@gmail.com>
2018-12-02 21:45:13 +0200

    Added app constants

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-// Reducer function - Todos
-function todos(state = [], action) {
-
-	switch (action.type) {
-		case 'ADD_TODO':
-			return state.concat([action.todo])
-		case 'REMOVE_TODO':
-			return state.filter((todo) => todo.id !== action.id)
-		case 'TOGGLE_TODO':
-			return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {
-				complete: !todo.complete
-			}))
-		default:
-			return state
-	}
-
-}
-
-// Reducer function - Goals
-function goals(state = [], action) {
-
-	switch (action.type) {
-		case 'ADD_GOAL':
-			return state.concat([action.todo])
-		case 'REMOVE_GOAL':
-			return state.filter((goal) => goal.id !== action.id)
-		default:
-			return state
-	}
-
-}
-
-// Root reducer function
-function app(state = {}, action) {
-
-	return {
-		todos: todos(state.todos, action),
-		goals: goals(state.goals, action),
-	}
-
-}
+/***** Library Code *****/
 
 function createStore(reducer) {
 	// The store should have four parts:
@@ -71,4 +31,55 @@ function createStore(reducer) {
 		subscribe,
 		dispatch,
 	}
+}
+
+/***** App Code *****/
+
+// App constants
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+// Reducer function - Todos
+function todos(state = [], action) {
+
+	switch (action.type) {
+		case ADD_TODO:
+			return state.concat([action.todo])
+		case REMOVE_TODO:
+			return state.filter((todo) => todo.id !== action.id)
+		case TOGGLE_TODO:
+			return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {
+				complete: !todo.complete
+			}))
+		default:
+			return state
+	}
+
+}
+
+// Reducer function - Goals
+function goals(state = [], action) {
+
+	switch (action.type) {
+		case ADD_GOAL:
+			return state.concat([action.todo])
+		case REMOVE_GOAL:
+			return state.filter((goal) => goal.id !== action.id)
+		default:
+			return state
+	}
+
+}
+
+// Root reducer function
+function app(state = {}, action) {
+
+	return {
+		todos: todos(state.todos, action),
+		goals: goals(state.goals, action),
+	}
+
 }
